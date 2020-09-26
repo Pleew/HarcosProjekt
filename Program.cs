50c365248e1ebc22441ec07db9394418c9a5103b
Pleew-HOME <ptl2217@gmail.com>
2020-09-26 18:12:38 +0200

    3/a

--- Program.cs
+++ Program.cs
@@ -12,28 +12,43 @@ namespace HarcosProjekt
     {
         static void Main(string[] args)
         {
+            listaBeEsKi();
+            
+
+            Console.WriteLine("Kérjem adja meg a harcosa nevét és státusz sablonját:\n");
+            Console.WriteLine("Neve:");
+            string jatekosHarcosNev = Console.ReadLine();
+            Console.WriteLine("\nStátusz sablonja:");
+            int jatekosStatus = Int32.Parse(Console.ReadLine());
+
+
+
+
+            Console.ReadLine();
+        }
+        private static void listaBeEsKi()
+        {
             List<string> harcosok = new List<string>();
             harcosok.Add("Jani");
-            harcosok.Add("2");
+            harcosok.Add("4");
             harcosok.Add("Paladino");
-            harcosok.Add("3");
+            harcosok.Add("5");
             harcosok.Add("Józsi");
-            harcosok.Add("1");
+            harcosok.Add("6");
 
             StreamReader sr = new StreamReader("harcosok.csv");
-			while (!sr.EndOfStream)
-			{
+            while (!sr.EndOfStream)
+            {
 
                 string[] beolvas = sr.ReadLine().Split(';');
                 harcosok.Add(beolvas[0]);
                 harcosok.Add(beolvas[1]);
 
             }
-			for (int i = 0; i < 12; i++)
-			{
-				Console.WriteLine(harcosok[i]);
-			}
-            Console.ReadLine();
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine(harcosok[i]);
+            }
         }
     }
 }
