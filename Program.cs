051d269592b458d1b10ba1c95049500fce460b61
Pleew-HOME <ptl2217@gmail.com>
2020-09-26 13:57:23 +0200

    2. feladat kész

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,28 @@ namespace HarcosProjekt
     {
         static void Main(string[] args)
         {
-           
+            List<string> harcosok = new List<string>();
+            harcosok.Add("Jani");
+            harcosok.Add("2");
+            harcosok.Add("Paladino");
+            harcosok.Add("3");
+            harcosok.Add("Józsi");
+            harcosok.Add("1");
+
+            StreamReader sr = new StreamReader("harcosok.csv");
+			while (!sr.EndOfStream)
+			{
+
+                string[] beolvas = sr.ReadLine().Split(';');
+                harcosok.Add(beolvas[0]);
+                harcosok.Add(beolvas[1]);
+
+            }
+			for (int i = 0; i < 12; i++)
+			{
+				Console.WriteLine(harcosok[i]);
+			}
+            Console.ReadLine();
         }
     }
 }
